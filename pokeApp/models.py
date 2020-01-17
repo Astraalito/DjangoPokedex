95cdfeacebaa0d5097e96c85fdd3dadc23e5f2af
Astraalito <nicolas.asri@epsi.fr>
2020-01-17 15:20:11 +0100

    feature(pokeApp): create 'Pokemon' Object

--- pokeApp/models.py
+++ pokeApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Pokemon(models.Model):
+    name = models.CharField(max_length=20)
+    number = models.IntegerField()
+    type = models.IntegerField()
+    imageLink = models.CharField(max_length=250)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
